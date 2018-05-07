7ab391cb48fd7d657a7a36cd237516520dec6c55
ArielFeingold <feingold.ariel@gmail.com>
2018-05-07 19:12:54 -0400

    added signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to edit_user_path(@user)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -17,4 +24,15 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :about,
+      )
+    end
 end
