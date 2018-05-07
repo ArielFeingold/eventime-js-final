d6bca203e472f0e3d97e1fa32524294ef0eca2ee
ArielFeingold <feingold.ariel@gmail.com>
2018-05-07 19:36:04 -0400

    added edit and update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,13 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find_by(id: session[:user_id])
   end
 
   def update
+    @user = User.find(params[:id])
+		@user.update(user_params)
+	  redirect_to user_path(@user)
   end
 
   def show
