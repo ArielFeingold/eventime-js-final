effb0ea225a343ba767ed318b39754d38c63d0f7
ArielFeingold <feingold.ariel@gmail.com>
2018-05-10 09:33:32 -0400

    added city & state attributes to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_08_185425) do
+ActiveRecord::Schema.define(version: 2018_05_10_133239) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 2018_05_08_185425) do
     t.string "about"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
+    t.string "state"
   end
 
 end
