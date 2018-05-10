f8818af9bb2efb72fd5a8daa790d8cf367d4c651
ArielFeingold <feingold.ariel@gmail.com>
2018-05-10 17:24:18 -0400

    added validation to event- date cannot be in the past

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,4 +9,11 @@ class Event < ApplicationRecord
   accepts_nested_attributes_for :rsvps
 
   validates :title, :description, :date, :time, presence: true
+  validate :event_date_cannot_be_in_the_past
+
+  def event_date_cannot_be_in_the_past
+    if self.date.present? && self.date < Date.today
+     errors.add(:past_date, "Event can't be in the past")
+   end
+  end
 end
