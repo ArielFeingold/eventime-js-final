6045b59d6fcdc3fe22f9b5b0e2f80d15f54fc903
ArielFeingold <feingold.ariel@gmail.com>
2018-05-10 16:15:57 -0400

    fixed redirect for Rsvp on event show page

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,17 +1,20 @@
 class RsvpsController < ApplicationController
 
   def create
+    @event = Event.find_by(id: params[:event_id])
     rsvp = Rsvp.new
     rsvp.user_id = current_user.id
     rsvp.event_id = params[:event_id]
     rsvp.save
-    redirect_to user_path(current_user)
+    redirect_to event_path(@event)
   end
 
   def destroy
+    @event = Event.find_by(id: params[:id])
     rsvp = Rsvp.find_by(user_id: current_user, event_id: params[:id])
     rsvp.destroy
-    redirect_to user_path(current_user)
+    binding.pry
+    redirect_to event_path(@event)
   end
 
 end
