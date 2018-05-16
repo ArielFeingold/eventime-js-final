56e849156b3cfbdce81cda16307192d82dc32031
ArielFeingold <feingold.ariel@gmail.com>
2018-05-16 08:47:18 -0400

    Fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,9 @@ jenny = User.create(name: "Jenny Folson", email: "jenny@gmail.com", password: "1
 paul = User.create(name: "Paul Bane", email: "paul@gmail.com", password: "12345", password_confirmation: "12345", about: "Love travel, friends and cakes!", city: "New York", state: "NY")
 rebecca = User.create(name: "Rebecca Strutt", email: "becky@gmail.com", password: "12345", password_confirmation: "12345", about: "nothing to see, walk away", city: "Los Angeles", state: "CA")
 
-radiohead = Event.create(title: "Radiohead Concert", description: "first live performance in 5 years", date: "2018-05-08", time: "2000-01-01 18:34:00", user_id: 1, location_id: 1)
-seinfeld = Event.create(title: "Seinfeld Live", description: "Live from MSG", date: "2018-07-08", time: "2000-01-01 18:34:00", user_id: 2, location_id: 1)
-birthday = Event.create(title: "Birthday", description: "My Bday", date: "2018-05-12", time: "2000-01-01 18:34:00", user_id: 3, location_id: 2)
-
 msg = Location.create(name:"Madison Sq. Garden", address: "325 7th Ave.", city: "New York", state: "NY")
 home = Location.create(name:"Becky's Home", address: "382 central Park West.", city: "New York", state: "NY")
+
+radiohead = Event.create(title: "Radiohead Concert", description: "first live performance in 5 years", date: "2018-05-25", time: "2000-01-01 18:34:00", user_id: 1, location_id: 1)
+seinfeld = Event.create(title: "Seinfeld Live", description: "Live from MSG", date: "2018-07-08", time: "2018-01-01 18:34:00", user_id: 2, location_id: 1)
+birthday = Event.create(title: "Birthday", description: "My Bday", date: "2018-07-22", time: "2000-01-01 18:34:00", user_id: 3, location_id: 2)
