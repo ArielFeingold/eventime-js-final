c97be2182617bf53f11611466a618123934c1343
ArielFeingold <feingold.ariel@gmail.com>
2018-07-25 15:59:23 -0400

    retriving current user id from #user_events.attr send get request to user#show to get user info

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,1 +1,11 @@
 
+$(function () {
+  const userId = $('#user_events').data('id');
+
+    $.get(`/users/${userId}.json`, function(data) {
+        debugger
+      user = data
+      userEvents = $(data.events).toArray();
+      userRsvps = $(data.rsvps).toArray();
+  })
+})
