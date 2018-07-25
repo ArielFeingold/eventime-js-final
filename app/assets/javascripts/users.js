841c23b1be9f42f35e75c5d2cc7998008198b17c
ArielFeingold <feingold.ariel@gmail.com>
2018-07-25 16:01:26 -0400

    added attachEvents function to append user events to his upcoming events list

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,9 +3,22 @@ $(function () {
   const userId = $('#user_events').data('id');
 
     $.get(`/users/${userId}.json`, function(data) {
-        debugger
       user = data
       userEvents = $(data.events).toArray();
       userRsvps = $(data.rsvps).toArray();
+
+      attachEvents(userEvents)
   })
+
+  function attachEvents(array) {
+  if(array.length) {
+    $(array).each(function(index, e) {
+      text = `<li class="list-group-item"><a href="/users/${userId}/events/${index}">${e.title}</a>, ${e.date}</li>`
+      $("#user_events").append(text);
+    })
+  } else {
+    text = "You have no upcoimng events"
+    $("#user_events").append(text);
+  }
+}
 })
